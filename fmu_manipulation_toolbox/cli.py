99364041555dae18c5083e2bfd81be235ef287c8
Nicolas Laurent <nl@green.lan>
2025-06-01 11:33:02 +0200

    Initial version of uncontainer tool

--- fmu_manipulation_toolbox/cli.py
+++ fmu_manipulation_toolbox/cli.py
@@ -5,6 +5,7 @@ from colorama import Fore, Style, init
 
 from .fmu_operations import *
 from .fmu_container import FMUContainerError
+from .uncontainer import UNcontainer, UNcontainerError
 from .assembly import Assembly, AssemblyError
 from .checker import checker_list
 from .version import __version__ as version
@@ -231,5 +232,39 @@ def fmucontainer():
             continue
 
 
+def uncontainer():
+    logger = setup_logger()
+
+    logger.info(f"UNcontainer version {version}")
+    parser = argparse.ArgumentParser(prog="uncontainer", description="Split FMU Container into FMU's",
+                                     formatter_class=make_wide(argparse.ArgumentDefaultsHelpFormatter),
+                                     add_help=False,
+                                     epilog="see: https://github.com/grouperenault/fmu_manipulation_toolbox/blob/main/"
+                                            "container/README.md")
+    parser.add_argument('-h', '-help', action="help")
+
+    parser.add_argument("-debug", action="store_true", dest="debug",
+                        help="Add lot of useful log during the process.")
+
+    parser.add_argument("-fmu", action="append", dest="fmu_filename_list", default=[],
+                        metavar="filename.fmu", required=True,
+                        help="Description of the FMU container to split.")
+
+    config = parser.parse_args()
+
+    if config.debug:
+        logger.setLevel(logging.DEBUG)
+
+    for fmu_filename in config.fmu_filename_list:
+        try:
+            uncontainer = UNcontainer(fmu_filename)
+            uncontainer.split_fmu()
+        except UNcontainerError as e:
+            logger.fatal(f"{fmu_filename}: {e}")
+            continue
+        except FileNotFoundError as e:
+            logger.fatal(f"Cannot read file: {e}")
+            continue
+
 if __name__ == "__main__":
-    fmucontainer()
+    uncontainer()
