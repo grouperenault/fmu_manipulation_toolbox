daa4d92d19e5bc931063e57fe309e00e9c33bf01
Nicolas Laurent <nl@green.lan>
2025-07-12 21:38:30 +0200

    First fmi3 embedded FMU working!

--- container/fmi3.c
+++ container/fmi3.c
@@ -49,11 +49,35 @@ fmi3Instance fmi3InstantiateCoSimulation(
     fmi3InstanceEnvironment        instanceEnvironment,
     fmi3LogMessageCallback         logMessage,
     fmi3IntermediateUpdateCallback intermediateUpdate) {
-        return NULL;
+    container_t* container;
+    container = container_new(instanceName, instantiationToken);
+
+    if (container) {
+        logger_function_t container_logger;
+        container_logger.logger_fmi3 = logMessage;
+        logger_init(FMU_3, container_logger, instanceEnvironment, container->instance_name, loggingOn); 
+        /* logger() is available starting this point ! */
+
+        logger(LOGGER_DEBUG, "Container model loading...");
+        if (strncmp(resourcePath, "file://", 7) == 0)
+            resourcePath += 7;
+
+        if (container_configure(container, resourcePath)) {
+            logger(LOGGER_ERROR, "Cannot read container configuration.");
+            container_free(container);
+            return NULL;
+        }
+        logger(LOGGER_DEBUG, "Container configuration read.");
     }
+    return container;
+}
+
 
 void fmi3FreeInstance(fmi3Instance instance) {
     container_t* container = (container_t*)instance;
+    
+    if (container)
+        container_free(container);
 
     return;
 }
@@ -64,13 +88,47 @@ fmi3Status fmi3EnterInitializationMode(fmi3Instance instance,
                                                    fmi3Float64 startTime,
                                                    fmi3Boolean stopTimeDefined,
                                                    fmi3Float64 stopTime) {
-    __NOT_IMPLEMENTED__
+    container_t* container = (container_t*)instance;
+
+    container->tolerance_defined = toleranceDefined;
+    container->tolerance = tolerance;
+    container->start_time = startTime;
+    container->stop_time_defined = 0; /* stopTime can cause rounding issues. Disbale it.*/
+    container->stop_time = stopTime;
+
+    for(int i=0; i < container->nb_fmu; i += 1) {
+        fmu_status_t status = fmuSetupExperiment(&container->fmu[i]);    
+        
+        if (status != FMU_STATUS_OK)
+            return fmi3OK;
+    }
+
+    container_set_start_values(container, 1);
+    logger(LOGGER_DEBUG, "fmuSetupExperiment -- OK");
+
+    for (int i = 0; i < container->nb_fmu; i += 1) {
+        fmu_status_t status = fmuEnterInitializationMode(&container->fmu[i]);
+        if (status != FMU_STATUS_OK)
+            return fmi3Error;
+    }
+
+    container_set_start_values(container, 0);
+
+    return fmi3OK;
 }
+
 
 fmi3Status fmi3ExitInitializationMode(fmi3Instance instance){
     container_t* container = (container_t*)instance;
 
-    __NOT_IMPLEMENTED__
+    for (int i = 0; i < container->nb_fmu; i += 1) {
+        fmu_status_t status = fmuExitInitializationMode(&container->fmu[i]);
+
+        if (status != FMU_STATUS_OK)
+            return fmi3Error;
+    }
+ 
+    return fmi3OK;
 }
 
 
@@ -84,125 +142,69 @@ fmi3Status fmi3EnterEventMode(fmi3Instance instance) {
 fmi3Status fmi3Terminate(fmi3Instance instance) {
     container_t* container = (container_t*)instance;
 
-    __NOT_IMPLEMENTED__
+    for (int i = 0; i < container->nb_fmu; i += 1) {
+        fmu_status_t status = fmuTerminate(&container->fmu[i]);
+
+        if (status != FMU_STATUS_OK)
+            return fmi3Error;
+    }
+ 
+    return fmi3OK;
 }
 
 
 fmi3Status fmi3Reset(fmi3Instance instance) {
     container_t* container = (container_t*)instance;
 
-    __NOT_IMPLEMENTED__
+    for (int i = 0; i < container->nb_fmu; i += 1) {
+        fmu_status_t status = fmuReset(&container->fmu[i]);
+
+        if (status != FMU_STATUS_OK)
+            return fmi3Error;
+    }
+ 
+    return fmi3OK;
+}
+
+/* Getting and setting variable values */
+#define FMI_GETTER(type, fmi_type, fmu_type) \
+fmi3Status fmi3Get ## fmi_type (fmi3Instance instance, const fmi3ValueReference valueReferences[], size_t nValueReferences, fmi3 ## fmi_type value[], size_t nValues) { \
+    container_t* container = (container_t*)instance; \
+    fmu_status_t status; \
+\
+    for (size_t i = 0; i < nValueReferences; i += 1) { \
+        const container_port_t *port = &container->port_ ##type [valueReferences[i]]; \
+        const int fmu_id = port->links[0].fmu_id; \
+\
+        if (fmu_id < 0) { \
+            value[i] = container-> type [valueReferences[i]]; \
+        } else { \
+            const fmu_vr_t fmu_vr = port->links[0].fmu_vr; \
+            const fmu_t *fmu = &container->fmu[fmu_id]; \
+\
+            status = fmuGet ## fmu_type (fmu, &fmu_vr, 1, &value[i]); \
+            if (status != FMU_STATUS_OK) \
+                return fmi3Error; \
+        } \
+    } \
+\
+    return fmi3OK; \
 }
 
 
-fmi3Status fmi3GetFloat32(fmi3Instance instance,
-                          const fmi3ValueReference valueReferences[],
-                          size_t nValueReferences,
-                          fmi3Float32 values[],
-                           size_t nValues)  {
-    container_t* container = (container_t*)instance;
+FMI_GETTER(reals64, Float64, Real);
+FMI_GETTER(reals32, Float32, Real); // TODO
+FMI_GETTER(integers8, Int8, Integer); // TODO
+FMI_GETTER(uintegers8, UInt8, Integer); // TODO
+FMI_GETTER(integers16, Int16, Integer); // TODO
+FMI_GETTER(uintegers16, UInt16, Integer); // TODO
+FMI_GETTER(integers32, Int32, Integer);
+FMI_GETTER(uintegers32, UInt32, Integer); // TODO
+FMI_GETTER(integers64, Int64, Integer); // TODO
+FMI_GETTER(uintegers64, UInt64, Integer); // TODO
 
-    __NOT_IMPLEMENTED__
-}
+#undef FMI_GETTER
 
-
-fmi3Status fmi3GetFloat64(fmi3Instance instance,
-                          const fmi3ValueReference valueReferences[],
-                          size_t nValueReferences,
-                        fmi3Float64 values[],
-                          size_t nValues)  {
-    container_t* container = (container_t*)instance;
-
-    __NOT_IMPLEMENTED__
-}
-
-
-fmi3Status fmi3GetInt8(fmi3Instance instance,
-                       const fmi3ValueReference valueReferences[],
-                       size_t nValueReferences,
-                       fmi3Int8 values[],
-                       size_t nValues) {
-    container_t* container = (container_t*)instance;
-
-    __NOT_IMPLEMENTED__
-}
-
-
-fmi3Status fmi3GetUInt8(fmi3Instance instance,
-                        const fmi3ValueReference valueReferences[],
-                        size_t nValueReferences,
-                        fmi3UInt8 values[],
-                        size_t nValues)  {
-    container_t* container = (container_t*)instance;
-
-    __NOT_IMPLEMENTED__
-}
-
-
-fmi3Status fmi3GetInt16(fmi3Instance instance,
-                        const fmi3ValueReference valueReferences[],
-                        size_t nValueReferences,
-                        fmi3Int16 values[],
-                        size_t nValues) {
-    container_t* container = (container_t*)instance;
-
-    __NOT_IMPLEMENTED__
-}
-
-
-fmi3Status fmi3GetUInt16(fmi3Instance instance,
-                         const fmi3ValueReference valueReferences[],
-                         size_t nValueReferences,
-                         fmi3UInt16 values[],
-                         size_t nValues) {
-    container_t* container = (container_t*)instance;
-
-    __NOT_IMPLEMENTED__
-}
-
-
-fmi3Status fmi3GetInt32(fmi3Instance instance,
-                        const fmi3ValueReference valueReferences[],
-                        size_t nValueReferences,
-                        fmi3Int32 values[],
-                        size_t nValues)  {
-    container_t* container = (container_t*)instance;
-
-    __NOT_IMPLEMENTED__
-}
-
-
-fmi3Status fmi3GetUInt32(fmi3Instance instance,
-                         const fmi3ValueReference valueReferences[],
-                         size_t nValueReferences,
-                         fmi3UInt32 values[],
-                         size_t nValues) {
-    container_t* container = (container_t*)instance;
-
-    __NOT_IMPLEMENTED__
-}
-
-
-fmi3Status fmi3GetInt64(fmi3Instance instance,
-                        const fmi3ValueReference valueReferences[],
-                        size_t nValueReferences,
-                        fmi3Int64 values[],
-                        size_t nValues) {
-    container_t* container = (container_t*)instance;
-
-    __NOT_IMPLEMENTED__
-}
-
-
-fmi3Status fmi3GetUInt64(fmi3Instance instance,
-                         const fmi3ValueReference valueReferences[],
-                         size_t nValueReferences,
-                         fmi3UInt64 values[],
-                         size_t nValues) {
-    container_t* container = (container_t*)instance;
-
-    __NOT_IMPLEMENTED__
-}
 
 
 fmi3Status fmi3GetBoolean(fmi3Instance instance,
@@ -655,6 +657,142 @@ fmi3Status fmi3DoStep(fmi3Instance instance,
                       fmi3Boolean* earlyReturn,
                       fmi3Float64* lastSuccessfulTime) {
     container_t* container = (container_t*)instance;
+    const double end_time = currentCommunicationPoint + communicationStepSize;
+    fmu_status_t status = FMU_STATUS_OK;
+    const double curent_time = container->start_time + container->time_step * container->nb_steps;
 
+
+    const int local_steps = (int)((end_time - curent_time + container->tolerance) / container->time_step);
+    
+    /*
+     * Early return if requested end_time is lower than next container time step.
+     */
+    if (local_steps == 0)
+        return fmi3OK;
+    
+    for(int i = 0; i < local_steps; i += 1) {
+        container->do_step(container);
+        container->nb_steps += 1;
+
+        if (status != FMU_STATUS_OK) {
+            logger(LOGGER_ERROR, "Container cannot DoStep.");
+            return fmi3Error;
+        }
+    }       
+
+    const double new_time = container->start_time + container->time_step * container->nb_steps;
+    if (fabs(end_time - new_time) > container->tolerance) {
+        logger(LOGGER_WARNING, "Container CommunicationStepSize should be divisible by %e. (currentCommunicationPoint=%e, container_time=%e, expected_time=%e, tolerance=%e, local_steps=%d, nb_steps=%lld)", 
+            container->time_step, currentCommunicationPoint, new_time, end_time, container->tolerance, local_steps, container->nb_steps);
+        return fmi3Warning;
+    }
+
+    return fmi3OK;
+}
+
+
+/*----------------------------------------------------------------------------
+          F M I 3   F U N C T I O N S   ( M O D E L E X C H A N G E )
+----------------------------------------------------------------------------*/
+fmi3Instance fmi3InstantiateModelExchange(
+    fmi3String                 instanceName,
+    fmi3String                 instantiationToken,
+    fmi3String                 resourcePath,
+    fmi3Boolean                visible,
+    fmi3Boolean                loggingOn,
+    fmi3InstanceEnvironment    instanceEnvironment,
+    fmi3LogMessageCallback     logMessage) {
+        return NULL;
+    }
+
+
+fmi3Status fmi3EnterContinuousTimeMode(fmi3Instance instance) {
     __NOT_IMPLEMENTED__
 }
+
+
+fmi3Status fmi3CompletedIntegratorStep(fmi3Instance instance,
+                                       fmi3Boolean  noSetFMUStatePriorToCurrentPoint,
+                                       fmi3Boolean* enterEventMode,
+                                       fmi3Boolean* terminateSimulation) {
+    __NOT_IMPLEMENTED__
+}
+
+
+fmi3Status fmi3SetTime(fmi3Instance instance, fmi3Float64 time) {
+    __NOT_IMPLEMENTED__
+}
+
+
+fmi3Status fmi3SetContinuousStates(fmi3Instance instance,
+                                   const fmi3Float64 continuousStates[],
+                                   size_t nContinuousStates) {
+    __NOT_IMPLEMENTED__
+}
+
+
+fmi3Status fmi3GetContinuousStateDerivatives(fmi3Instance instance,
+                                             fmi3Float64 derivatives[],
+                                            size_t nContinuousStates) {
+    __NOT_IMPLEMENTED__
+}
+
+
+fmi3Status fmi3GetEventIndicators(fmi3Instance instance,
+                                  fmi3Float64 eventIndicators[],
+                                  size_t nEventIndicators) {
+    __NOT_IMPLEMENTED__
+}
+
+
+fmi3Status fmi3GetContinuousStates(fmi3Instance instance,
+                                   fmi3Float64 continuousStates[],
+                                   size_t nContinuousStates)  {
+    __NOT_IMPLEMENTED__
+}
+
+
+fmi3Status fmi3GetNominalsOfContinuousStates(fmi3Instance instance,
+                                             fmi3Float64 nominals[],
+                                             size_t nContinuousStates) {
+    __NOT_IMPLEMENTED__
+}
+
+
+
+fmi3Status fmi3GetNumberOfEventIndicators(fmi3Instance instance,
+                                          size_t* nEventIndicators)  {
+    __NOT_IMPLEMENTED__
+}
+
+
+
+fmi3Status fmi3GetNumberOfContinuousStates(fmi3Instance instance,
+                                           size_t* nContinuousStates) {
+    __NOT_IMPLEMENTED__
+}
+
+
+/*----------------------------------------------------------------------------
+    F M I 3   F U N C T I O N S   ( S C H E D U L E D   E X E C U T I O N )
+----------------------------------------------------------------------------*/
+fmi3Instance fmi3InstantiateScheduledExecution(
+    fmi3String                     instanceName,
+    fmi3String                     instantiationToken,
+    fmi3String                     resourcePath,
+    fmi3Boolean                    visible,
+    fmi3Boolean                    loggingOn,
+    fmi3InstanceEnvironment        instanceEnvironment,
+    fmi3LogMessageCallback         logMessage,
+    fmi3ClockUpdateCallback        clockUpdate,
+    fmi3LockPreemptionCallback     lockPreemption,
+    fmi3UnlockPreemptionCallback   unlockPreemption) {
+    return NULL;
+}
+
+fmi3Status fmi3ActivateModelPartition(fmi3Instance instance,
+                                      fmi3ValueReference clockReference,
+                                      fmi3Float64 activationTime) {
+    __NOT_IMPLEMENTED__
+}
+
