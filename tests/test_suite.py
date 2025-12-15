f2e1ec0fe2804c941bd4c5606114b3fc1c4caa57
Nicolas LAURENT <nl@haplo.info>
2025-12-15 18:57:33 +0100

    testsuite launch subprocess to avoid logging configuration collisions

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -3,15 +3,13 @@ import sys
 from pathlib import Path
 from fmpy.simulation import simulate_fmu
 import numpy as np
+import subprocess
 
 sys.path.insert(0, str(Path(__file__).parent.parent))
 from fmu_manipulation_toolbox.operations import *
 from fmu_manipulation_toolbox.remoting import *
 from fmu_manipulation_toolbox.container import *
 from fmu_manipulation_toolbox.assembly import *
-from fmu_manipulation_toolbox.cli.fmutool import fmutool
-from fmu_manipulation_toolbox.cli.fmusplit import fmusplit
-from fmu_manipulation_toolbox.cli.fmucontainer import fmucontainer
 
 class FMUManipulationToolboxTestSuite(unittest.TestCase):
     def __init__(self, *args, **kwargs):
@@ -212,6 +210,7 @@ class FMUManipulationToolboxTestSuite(unittest.TestCase):
         assembly.make_fmu()
         self.assert_simulation("containers/VanDerPol/VanDerPol-Container.fmu", 0.1)
 
+
     def test_container_vanderpol_vr(self):
         assembly = Assembly("VanDerPol-vr.json", fmu_directory=Path("containers/VanDerPol"))
         assembly.make_fmu()
@@ -245,27 +244,33 @@ class FMUManipulationToolboxTestSuite(unittest.TestCase):
             print(f"{link}")
 
     def test_fmutool(self):
-        sys.argv = ['fmutool', '-input', 'operations/bouncing_ball.fmu', '-summary', '-check', '-dump-csv',
+
+        argv = [sys.executable, '-mfmu_manipulation_toolbox.cli.fmutool',
+                '-input', 'operations/bouncing_ball.fmu', '-summary', '-check', '-dump-csv',
                  'operations/cli-bouncing_ball.csv']
-        fmutool()
+        subprocess.run(argv)
+
         self.assert_identical_files("operations/cli-bouncing_ball.csv", "operations/REF-bouncing_ball.csv")
 
     def test_fmucontainer_csv(self):
-        sys.argv = ['fmucontainer', '-container', 'cli-bouncing.csv', '-fmu-directory', 'containers/bouncing_ball',
-                    '-mt', '-debug']
-        fmucontainer()
+        argv = [sys.executable, '-mfmu_manipulation_toolbox.cli.fmucontainer',
+                '-container', 'cli-bouncing.csv', '-fmu-directory', 'containers/bouncing_ball',
+                '-mt', '-debug']
+        subprocess.run(argv)
         self.assert_identical_files("containers/bouncing_ball/REF-container.txt",
                                     "containers/bouncing_ball/cli-bouncing/resources/container.txt")
 
     def test_fmucontainer_json(self):
-        sys.argv = ['fmucontainer', '-fmu-directory', 'containers/arch', '-container', 'cli-flat.json', '-dump']
-        fmucontainer()
+        argv = [sys.executable, '-mfmu_manipulation_toolbox.cli.fmucontainer',
+                '-fmu-directory', 'containers/arch', '-container', 'cli-flat.json', '-dump']
+        subprocess.run(argv)
         self.assert_identical_files("containers/arch/REF-cli-flat-dump.json",
                                     "containers/arch/cli-flat-dump.json")
 
     def test_fmusplit(self):
-        sys.argv = ["fmusplit", "-fmu", "containers/ssp/bouncing.fmu"]
-        fmusplit()
+        argv = [sys.executable, '-mfmu_manipulation_toolbox.cli.fmusplit',
+                "-fmu", "containers/ssp/bouncing.fmu"]
+        subprocess.run(argv)
         self.assertTrue(Path("containers/ssp/bouncing.dir/bb_position.fmu").exists())
         self.assertTrue(Path("containers/ssp/bouncing.dir/bb_velocity.fmu").exists())
         self.assert_identical_files("containers/ssp/REF-split-bouncing.json",
