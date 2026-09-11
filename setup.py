83aee0246ccae69bcefc2d240c9cb8488f3f1c8b
Nicolas LAURENT <nl@haplo.info>
2026-09-11 09:48:09 +0200

    First experimentation

--- setup.py
+++ setup.py
@@ -89,6 +89,7 @@ FMI versions 2.0, 3.0 and LS-BUS are supported.
         "xmlschema >= 3.3.1",
         "elementpath >= 4.4.0",
         "colorama >= 0.4.6",
+        "fastmcp >= 2.14.0; python_version>='3.10'",
         "importlib_metadata >= 8.7.0; python_version<'3.10'"
     ],
     extras_require={
